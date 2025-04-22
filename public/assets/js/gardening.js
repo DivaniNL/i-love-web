047f05fbb7f8d7d6c46615e5fddbe498ec7b21ee
DivaniNL <dylanvannierop@gmail.com>
2025-04-22 10:34:01 +0200

    added two garden experiments

---
+++ public/assets/js/gardening.js
@@ -0,0 +1,8 @@
+// Layout shift
+
+document.querySelector('.layout-shift .destroy').addEventListener('click', function(){
+    alert("De hele wereld gaat naar de klote en het is JOUW schuld!!")
+})
+
+//
+
